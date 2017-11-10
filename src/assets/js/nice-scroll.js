0511359390841d86adfe240fc3d1ec5e7c6a7321
Mariano Antonio Soto Hernandez <mantiniok0051@gmail.com>
2017-11-09 23:38:21 -0600

    Add files via upload

---
+++ src/assets/js/nice-scroll.js
@@ -0,0 +1,61 @@
+        // Nice-Scroll
+
+        $("html").niceScroll({
+            cursorborder: 'transparent',
+            cursorborderradius: '0px', // Scroll cursor radius
+            cursorwidth: '5px',       // Scroll cursor width
+            cursorcolor: 'transparent'     // Scroll cursor color
+            });
+
+
+
+        // Nice-Scroll ModalQuote
+
+        $("#myModalQuote").niceScroll({
+            cursorborder: 'transparent',
+            cursorborderradius: '0px', // Scroll cursor radius
+            cursorwidth: '5px',       // Scroll cursor width
+            cursorcolor: 'transparent'     // Scroll cursor color
+            });
+
+
+        // Nice-Scroll Modal
+
+        $("#myModal").niceScroll({
+            cursorborder: 'transparent',
+            cursorborderradius: '0px', // Scroll cursor radius
+            cursorwidth: '5px',       // Scroll cursor width
+            cursorcolor: 'transparent'     // Scroll cursor color
+            });
+        $("#mydiv").getNiceScroll().hide();
+        $("#mydiv").getNiceScroll().resize();
+
+        // Nice-Scroll Modal Services
+
+        $("#myModalApp").niceScroll({
+            cursorborder: 'transparent',
+            cursorborderradius: '0px', // Scroll cursor radius
+            cursorwidth: '5px',       // Scroll cursor width
+            cursorcolor: 'transparent'     // Scroll cursor color
+            });
+        $("#mydiv").getNiceScroll().hide();
+
+        // Nice-Scroll Modal Services
+
+        $("#myModalDeb").niceScroll({
+            cursorborder: 'transparent',
+            cursorborderradius: '0px', // Scroll cursor radius
+            cursorwidth: '5px',       // Scroll cursor width
+            cursorcolor: 'transparent'     // Scroll cursor color
+            });
+        $("#mydiv").getNiceScroll().hide();
+
+        // Nice-Scroll Modal Services
+
+        $("#myModalSoft").niceScroll({
+            cursorborder: 'transparent',
+            cursorborderradius: '0px', // Scroll cursor radius
+            cursorwidth: '5px',       // Scroll cursor width
+            cursorcolor: 'transparent'     // Scroll cursor color
+            });
+        $("#mydiv").getNiceScroll().hide();
